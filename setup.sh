ab6f25dcee816adb4bc9b46789ac04a31f4ee611
Abdullah Albassami <albassamiabdulla@gmail.com>
2026-08-02 18:40:30 +0300

    Initial Commit

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ touch project/main.py project/README.md
 # Filling README.md file
 echo "1- created the setup.sh shell script file
 2- Accessed the script file using nano
-3- Created the commands in order to firstr create the project folder, create two files (main.py, README.md), documented the steps in the README.md file, initialized git, added Created Files, Commited changes, pushed the project on github, displayed a success message, fianlly added excute mode to the script file using chmod +x setup.sh" >> README.md 
+3- Created the commands in order to firstr create the project folder, create two files (main.py, README.md), documented the steps in the README.md file, initialized git, added Created Files, Commited changes, pushed the project on github, displayed a success message, fianlly added excute mode to the script file using source setup.sh" >> README.md 
 
 # Initialize Git
 git init

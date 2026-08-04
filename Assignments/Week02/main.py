16a07865153d04bae29356a7d37ec97af60865df
Abdullah Albassami <albassamiabdulla@gmail.com>
2026-08-04 14:26:33 +0300

    Created Week02 folder inside the Assignments folder and create a main.py file for student card assignment

---
+++ Assignments/Week02/main.py
@@ -0,0 +1,22 @@
+
+student_name = "Abdullah"
+student_age = 26
+course = "Python"
+isRegistered = True
+
+status = "Registered" if isRegistered else "Not Registered"
+
+print(f"""
+Name: {student_name}
+Program: {course}
+Age: {student_age}
+Registration: {status}
+""")
+
+print(f"""
+Welcome {student_name} to the {course} course!
+You are {student_age} years old
+Your current registration status is: {status}
+Thank you {student_name} and best of luck!
+""")
+
---
+++ project/main.py
@@ -0,0 +1,9 @@
+student_name = "Abdullah"
+student_age = 26
+course = "Python"
+
+
+print(f"""
+Welcome {student_name} to the {course} course!
+you are {student_age} years old
+""")

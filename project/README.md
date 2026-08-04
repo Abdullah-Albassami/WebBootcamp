d8277085773f146470aa7a8753386846d73c2017
Abdullah Albassami <albassamiabdulla@gmail.com>
2026-08-05 01:41:13 +0300

    Initial Commit

--- project/README.md
+++ project/README.md
@@ -10,3 +10,14 @@
 8- Pushed the project on github 
 9- Displayed a success message 
 10- Made the script executable using chmod +x setup.sh.
+
+1. Created the setup.sh shell script file
+2. Accessed the script file using nano
+3. Created the project folder
+4. Created main.py and README.md
+5. Documented the steps in README.md
+6. Initialized Git
+7. Added the created files
+8. Committed the changes
+9. Pushed the project to GitHub
+10. Made setup.sh executable using chmod +x setup.sh
--- setup.sh
+++ setup.sh
@@ -10,17 +10,16 @@ touch project/main.py project/README.md
 
 # Filling README.md file
 echo "
-1- created the setup.sh shell script file
-2- Accessed the script file using nano
-3- Created the commands in order to firstr create the project folder
-4- Create two files (main.py, README.md)
-5- Documented the steps in the README.md file
-6- Initialized git
-7- Added Created Files 
-7- Commited changes
-8- Pushed the project on github 
-9- Displayed a success message 
-10- Made the script executable using chmod +x setup.sh." >> project/README.md 
+1. Created the setup.sh shell script file
+2. Accessed the script file using nano
+3. Created the project folder
+4. Created main.py and README.md
+5. Documented the steps in README.md
+6. Initialized Git
+7. Added the created files
+8. Committed the changes
+9. Pushed the project to GitHub
+10. Made setup.sh executable using chmod +x setup.sh" >> project/README.md 
 
 # Initialize Git
 git init

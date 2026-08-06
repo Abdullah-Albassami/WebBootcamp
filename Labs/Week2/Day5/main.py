6ba089c33e81c542f68f382039a0da383362903b
Abdullah Albassami <albassamiabdulla@gmail.com>
2026-08-06 22:19:40 +0300

    Added Day 5 labs

--- Labs/Week2/Day5/main.py
+++ Labs/Week2/Day5/main.py
@@ -52,13 +52,60 @@ print(f"total number of iter is {count}") """
 # for letter in course:
 #     print(letter)
 
-# LAB 5 
-students = ["shahad", "a", "b", "c", "d"]
-for student in students:
-    print(f"Progressing student is: {student}")
+# # LAB 5 
+# students = ["shahad", "a", "b", "c", "d"]
+# for student in students:
+#     print(f"Progressing student is: {student}")
 
-# LAB 6 
+# # LAB 6 
+# for number in range(1, 11):
+#     if number % 2 == 0:
+#         print(f"{number} is even")
+#     else:
+#         print(f"{number} is odd")
+#     print("------------")
+# # LAB 7
+# numbers = [4, 7, 10, 13, 16, 21]
+# even_counter = 0
 
+# for num in numbers:
+#     if num % 2 == 0:
+#         even_counter += 1 
+
+# print(f"Total even numbers is: {even_counter}")
+
+# # LAB 8
+# prices = [25, 30, 55, 115]
+# total = 0
+
+# for price in prices:
+#     total += price
+
+# print(f"your total is {total} VAT: {total * (15/100)}") # (1.15:.2f) to show only two decimals
+
+# # LAB 9
+# count = 0
+# while count <= 5:
+#     count += 1
+#     print(f"Count...{count}")
+# print("Loop Completed!")
+
+# # LAB 10
+# message = "Please enter a number: "
+# age_text = input("Please enetr your age: ").strip()
+# while not age_text.isdigit():
+#     age_text = input(message).strip()
+
+# age = int(age_text)
+
+# print(f"you are {age} yours old")
+
+# # LAB 11
+# password = "python123"
+# print("Please Enter your password")
+
+# while password != "":
+#     password = input("Enter your password: ")
     
 
 
